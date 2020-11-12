dde7f7e5ef3618532f0421b51ad421ebf67ca66d
Alex Miller <asmiller1989@gmail.com>
2020-11-12 16:04:19 -0600

    Adds lattice-connector to example app

--- example/connectors.ts
+++ example/connectors.ts
@@ -11,6 +11,7 @@ import { MagicConnector } from '@web3-react/magic-connector'
 import { PortisConnector } from '@web3-react/portis-connector'
 import { SquarelinkConnector } from '@web3-react/squarelink-connector'
 import { TorusConnector } from '@web3-react/torus-connector'
+import { LatticeConnector } from '../packages/lattice-connector'
 
 const POLLING_INTERVAL = 12000
 const RPC_URLS: { [chainId: number]: string } = {
@@ -45,6 +46,12 @@ export const trezor = new TrezorConnector({
   pollingInterval: POLLING_INTERVAL,
   manifestEmail: 'dummy@abc.xyz',
   manifestAppUrl: 'http://localhost:1234'
+})
+
+export const lattice = new LatticeConnector({
+  chainId: 4,
+  appName: 'web3-react',
+  url: RPC_URLS[4],
 })
 
 export const frame = new FrameConnector({ supportedChainIds: [1] })
